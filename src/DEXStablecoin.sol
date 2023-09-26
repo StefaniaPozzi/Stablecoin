a039841106fa90954971ceec38994c90231c77db
StefaniaPozzi <stefania.pozzi.it@gmail.com>
2023-09-26 07:16:34 +0200

    init

---
+++ src/DEXStablecoin.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.18;
+
+
+/**
+ * @title DEXStablecoin
+ * @author Stefania Pozzi
+ * Collaterall: ETH, BTC
+ * Minting-burning: Algorithmic (DEXSEngine)
+ * Relative stability: USD
+ * 
+ * 
+*/
+contract DEXStablecoin {
+
+
+}
